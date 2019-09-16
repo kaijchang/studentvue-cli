7d9f55b3b29c2cb16580b684efea91f597d4f1bd
Kai Chang <kaijchang@gmail.com>
2019-09-16 07:57:07 -0700

    load class_info on demand

--- cli/main.py
+++ cli/main.py
@@ -10,23 +10,33 @@ from .Marquee import Marquee
 
 
 def run_menu(studentvue, stop_event, screen):
-    schedule_menu_items = []
+    cache = {}
+
+    def load_class_info(class_):
+        def wrapped():
+            start_load(screen)
+            if class_.name in cache:
+                cache[class_.name].display()
+            else:
+                class_info = studentvue.get_class_info(class_)
+                class_menu_items = []
+                if class_info is not None:
+                    for assignment in class_info['assignments']:
+                        assignment_menu_items = [
+                            ('Name: %s' % assignment.name, lambda: None),
+                            ('Due Date: %s' % assignment.date.strftime('%x'), lambda: None),
+                            ('Score: %s/%s' % (assignment.score, assignment.max_score), lambda: None)
+                        ]
+                        assignment_menu = Menu(assignment_menu_items, stop_event, screen, submenu=True)
+                        class_menu_items.append(('%s: %s' % (assignment.date.strftime('%x'), assignment.name),
+                                                 assignment_menu.display))
+                class_menu = Menu(class_menu_items, stop_event, screen, submenu=True)
+                cache[class_.name] = class_menu
+                class_menu.display()
+        return wrapped
+
     schedule = studentvue.get_schedule()
-    for class_ in schedule:
-        class_info = studentvue.get_class_info(class_)
-        class_menu_items = []
-        if class_info is not None:
-            for assignment in class_info['assignments']:
-                assignment_menu_items = [
-                    ('Name: %s' % assignment.name, lambda: None),
-                    ('Due Date: %s' % assignment.date.strftime('%x'), lambda: None),
-                    ('Score: %s/%s' % (assignment.score, assignment.max_score), lambda: None)
-                ]
-                assignment_menu = Menu(assignment_menu_items, stop_event, screen, submenu=True)
-                class_menu_items.append(('%s: %s' % (assignment.date.strftime('%x'), assignment.name),
-                                         assignment_menu.display))
-        class_menu = Menu(class_menu_items, stop_event, screen, submenu=True)
-        schedule_menu_items.append(('%s: %s' % (class_.period, class_.name), class_menu.display))
+    schedule_menu_items = [('%s: %s' % (class_.period, class_.name), load_class_info(class_)) for class_ in schedule]
 
     schedule_menu = Menu(schedule_menu_items, stop_event, screen, submenu=True)
 
@@ -44,17 +54,21 @@ def run_marquee(studentvue, stop_event, screen):
     marquee.display()
 
 
+def start_load(stdscreen):
+    y, x = stdscreen.getmaxyx()
+    stdscreen.addstr(round(y / 3), round(x / 2), 'Loading...', curses.color_pair(1))
+    stdscreen.refresh()
+
+
 class Jasper:
     def __init__(self, stdscreen):
         self.screen = stdscreen
         curses.curs_set(0)
-        stdscreen.scrollok(False)
+        stdscreen.scrollok(0)
         stdscreen.keypad(True)
 
-        y, x = stdscreen.getmaxyx()
         curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
-        stdscreen.addstr(round(y / 3), round(x / 2), 'Loading...', curses.color_pair(1))
-        stdscreen.refresh()
+        start_load(stdscreen)
 
         credentials = (os.environ['STUDENTVUE_USER'], os.environ['STUDENTVUE_PASS'], os.environ['STUDENTVUE_DOMAIN'])
         studentvue = StudentVue(*credentials)
